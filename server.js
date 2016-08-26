740d775a0049870a04f1991134a2975c460684a3
Doug <njdrl14@gmail.com>
2016-08-25 21:07:22 -0400

    first commit

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
-app.use('/', gameRoutee);
+app.use('/', gameRoute);
 
 app.listen(PORT, function() {
 	console.log('connected on port ' + PORT);
