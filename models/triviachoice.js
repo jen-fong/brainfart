c84e77af13714a11cf929db5f374516e733f0c5d
Jenny <jennyfong1015@gmail.com>
2016-08-30 16:59:05 -0400

    working game

--- models/triviachoice.js
+++ models/triviachoice.js
@@ -5,7 +5,7 @@ module.exports = function(sequelize, DataTypes) {
     status: {
       type: DataTypes.BOOLEAN,
       defaultValue: false
-    },
+    }
   }, {
     classMethods: {
       associate: function(models) {
