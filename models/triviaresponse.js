feadd9405d8e1d8b549a3e11ae42adad7f830963
Jenny <jennyfong1015@gmail.com>
2016-08-30 17:07:26 -0400

    working game

--- models/triviaresponse.js
+++ models/triviaresponse.js
@@ -1,18 +1,10 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
-<<<<<<< HEAD:models/triviachoice.js
-  var triviaChoice = sequelize.define('triviaChoice', {
-    choice: DataTypes.TEXT,
-    status: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
-=======
   var triviaResponse = sequelize.define('triviaResponse', {
     choice: DataTypes.TEXT,
     status: {
       type: DataTypes.BOOLEAN,
       defaultValue: true
->>>>>>> jennysocketbranch:models/triviaresponse.js
     }
   }, {
     classMethods: {
